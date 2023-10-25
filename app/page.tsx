7b69f93262048394ea771b33a1ac2fd100c2dbd7
Mike DiDomizio <mikedidomizio@gmail.com>
2023-10-25 15:06:21 +0000

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 // this by default is a server component
 export default function Home() {
-  // this is the culprit right here (todo this fails building though)
+  // this is the culprit right here
+  // note: this fails a production build in Codesandbox but builds fine when checked out
+  // confirmed that production build does in fact still leak it
   const { CSB_EXEC_ID } = process.env;
 
   async function changeUserName() {
@@ -11,7 +13,10 @@ export default function Home() {
 
   return (
     <form action={changeUserName as unknown as string}>
-      No leak
+      Right click and inspect this to see the environment variable leaked.
+      <br />
+      Inside the form is input with name `$ACTION_0:1`, this is the environment
+      variable above
       <br />
       <input
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
